0fbd71cda7c91c150c97a77964aeda1a08814247
dangerfrog <80552727+dangerfrog@users.noreply.github.com>
2021-12-28 13:30:46 +1300

    sorted svg placement

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import React from "react";
-import { Grommet } from "grommet";
+import { Box, Grommet } from "grommet";
 import PlaceHolderNav from "./Components/PlaceHolderNav";
 import MainBody from "./Components/MainBody";
+import "./App.css";
+import Background from "./Components/Background";
 
 const theme = {
   global: {
@@ -16,9 +18,29 @@ const theme = {
 function App() {
   return (
     <>
-      <Grommet theme={theme}>
+      <Grommet
+        theme={theme}
+        style={{
+          height: "100vh",
+          width: "100vw",
+          display: "flex",
+          flexDirection: "column",
+        }}
+      >
         <PlaceHolderNav />
         <MainBody />
+        <Box
+          style={{
+            position: "absolute",
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 0,
+            zIndex: -1,
+          }}
+        >
+          <Background />
+        </Box>
       </Grommet>
     </>
   );
--- src/Components/MainBody.tsx
+++ src/Components/MainBody.tsx
@@ -3,17 +3,23 @@ import { Box, Image, Grid, Paragraph } from "grommet";
 import { TreeOption } from "grommet-icons";
 
 const MainBody = () => (
-  <Box direction="row" align="between" justify="around" margin="none">
+  <Box direction="row" align="between" justify="around" margin="none" flex>
     <Box
       alignSelf="center"
-      flex="grow"
+      flex
       pad={{ top: "small", bottom: "xlarge" }}
       margin={{ horizontal: "xlarge", vertical: "none" }}
     >
-      <Image fill="vertical" src="/assets/HX2.svg" />
+      <Image height="80%" src="/assets/HX2.svg" />
     </Box>
-    <Box alignSelf="center" flex="grow" align="end">
-      <Image fill="vertical" src="/assets/HX1.svg" />
+    <Box alignSelf="center" align="end" flex>
+      <img
+        style={{
+          height: "calc(100vh - 48px)",
+          filter: "drop-shadow(4px 4px 3px rgba(0,0,0,0.4)) ",
+        }}
+        src="/assets/HX1.svg"
+      />
     </Box>
   </Box>
 );
