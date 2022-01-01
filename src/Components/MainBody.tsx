6d504703db7b651f8a1997fd0fa302d2d43395e3
dangerfrog <80552727+dangerfrog@users.noreply.github.com>
2022-01-01 17:27:02 +1300

    added media query to MainBody.tsx

--- src/Components/MainBody.tsx
+++ src/Components/MainBody.tsx
@@ -1,33 +1,47 @@
 /* Hero Images */
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Box, Image, Grid, Paragraph, ResponsiveContext } from "grommet";
 import { TreeOption } from "grommet-icons";
 
-const MainBody = () => (
-  <Box direction="row" align="between" justify="around" margin="none" flex>
-    <Box
-      flex
-      pad={{ top: "medium", bottom: "xlarge" }}
-      margin={{ horizontal: "xlarge", vertical: "none" }}
-    >
-      <Image
-        style={{
-          height: "calc(70vh - 22px)",
-        }}
-        src="/assets/HX2.svg"
-      />
-    </Box>
+const MainBody = () => {
+  const [matches, setMatches] = useState(
+    window.matchMedia("(min-width: 800px)").matches
+  );
 
-    <Box align="end" flex>
-      <Image
-        style={{
-          height: "calc(98vh - 54px)",
-          filter: "drop-shadow(4px 4px 3px rgba(0,0,0,0.4)) ",
-        }}
-        src="/assets/HX1.svg"
-      />
+  useEffect(() => {
+    window
+      .matchMedia("(min-width: 800px)")
+      .addEventListener("change", (e) => setMatches(e.matches));
+  }, []);
+
+  return (
+    <Box direction="row" align="between" justify="around" margin="none" flex>
+      <Box
+        flex
+        pad={{ top: "medium", bottom: "xlarge" }}
+        margin={{ horizontal: "xlarge", vertical: "none" }}
+      >
+        <Image
+          style={{
+            height: "calc(70vh - 22px)",
+          }}
+          src="/assets/HX2.svg"
+        />
+      </Box>
+      {matches && (
+        <Box align="end" flex>
+          <Image
+            style={{
+              height: "calc(98vh - 54px)",
+              filter: "drop-shadow(4px 4px 3px rgba(0,0,0,0.4)) ",
+            }}
+            src="/assets/HX1.svg"
+          />
+        </Box>
+      )}
+      {!matches}
     </Box>
-  </Box>
-);
+  );
+};
 
 export default MainBody;
